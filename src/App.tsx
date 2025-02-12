970d26a71cb854b5ed11244e3897a31bb798e63e
madhusudhanyella8 <161793236+madhusudhanyella8@users.noreply.github.com>
2025-02-12 13:00:33 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable react-refresh/only-export-components */
+
+export default () => {
+  return (
+    <div>
+      <iframe
+        src="/booth-doc.pdf#toolbar=0"
+        style={{ height: "100vh", width: "100vw" }}
+      />
+    </div>
+  );
+};
